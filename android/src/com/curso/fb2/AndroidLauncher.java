0926692f75712a405f55c6cc9b48aeee0ac08b25
matheusgomes2020 <matheusgomez2013@gmail.com>
2022-09-29 00:40:13 -0300

    Initial commit

---
+++ android/src/com/curso/fb2/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.curso.fb2;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.curso.fb2.Jogo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Jogo(), config);
+	}
+}
